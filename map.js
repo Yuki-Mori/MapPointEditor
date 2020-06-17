04c20805f742fb39a079adc015a5730c0c072fd0
Yuki-Mori <yuki7291@pluslab.org>
2020-06-17 13:38:12 +0900

    first init

---
+++ map.js
@@ -0,0 +1,3 @@
+(function(){
+    var mymap = L.map('mapid').setView([51.505, -0.09], 13);
+})()
---
+++ script.js
@@ -0,0 +1,18 @@
+(function() {
+    // Aタグのhref属性に、Blobを設定する。
+    // Blobに含めるデータは、引数で指定する。
+    function setBlobUrl(id, content) {
+        // 指定されたデータを保持するBlobを作成する。
+        var blob = new Blob([ content ], { "type" : "text/csv" });
+
+        // Aタグのhref属性にBlobオブジェクトを設定する。
+        window.URL = window.URL || window.webkitURL;
+        $("#" + id).attr("href", window.URL.createObjectURL(blob));
+    }
+
+    $("#save").click(function(){
+        setBlobUrl("download", $("#content").val());
+    });
+
+    $("#content").keyup();
+})();
