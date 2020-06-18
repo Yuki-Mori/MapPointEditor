233d25c8b69034f63aead7b3575a65a4bc7aa519
Yuki-Mori <yuki7291@pluslab.org>
2020-06-18 13:24:30 +0900

    Add a function that lat-lon data is obtained as a csv file.

--- map.js
+++ map.js
@@ -1,3 +1,14 @@
-(function(){
-    var mymap = L.map('mapid').setView([51.505, -0.09], 13);
-})()
+var mymap = L.map('mapid').setView([35.16386051866648,136.9636130332947], 16);
+L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
+    maxZoom: 18,
+    attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, '
+}).addTo(mymap);
+
+mymap.on('click', function(e){
+    var lat = e.latlng.lat;
+    var lon = e.latlng.lng;
+    $("#content").val($("#content").val() + '\n' + lat + ',' + lon);
+    $("#download").removeAttr("href");
+    $("#download").addClass("no-link")
+    //alert(lat + ',' + lon);
+});
--- script.js
+++ script.js
@@ -1,18 +1,19 @@
-(function() {
+$(function () {
     // Aタグのhref属性に、Blobを設定する。
     // Blobに含めるデータは、引数で指定する。
     function setBlobUrl(id, content) {
         // 指定されたデータを保持するBlobを作成する。
-        var blob = new Blob([ content ], { "type" : "text/csv" });
-
+        var blob = new Blob([content], {"type": "text/csv"});
         // Aタグのhref属性にBlobオブジェクトを設定する。
         window.URL = window.URL || window.webkitURL;
         $("#" + id).attr("href", window.URL.createObjectURL(blob));
     }
 
-    $("#save").click(function(){
+    $("#save").click(function () {
         setBlobUrl("download", $("#content").val());
+        if($("#download").hasClass("no-link")){
+            $("#download").removeClass("no-link");
+        }
     });
-
     $("#content").keyup();
-})();
+});
